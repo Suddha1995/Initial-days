c735882021bd44b61b89193ffaedaed8ad43e7c7
Suddha1995 <154587820+Suddha1995@users.noreply.github.com>
2024-08-05 12:10:37 +0530

    Add files via upload

---
+++ Python_Youtube_Training/L3_Tuples_n_List.py
@@ -0,0 +1,25 @@
+# List - is a built in data type that stores set of values. It can store elements of different types (integer, float, string, etc.)
+# Apply [] brackets to store multiple list of values.
+
+marks = [94,52,64,48,63,57]
+print(marks)
+print(type(marks))
+
+# List is identified as a separate class.
+
+print(marks[2])
+print(marks[4])
+print(len(marks))
+
+
+# Strings are "IMMUTABLE" and lists are "MUTABLE" in python. Which means MUTABLE can be changed but IMMUTABLE can't.
+# In index - positioning values in string, we could access but NOT change values, but we can both access and change values in list.
+"""
+List Slicing - similar to string slicing.
+list_name[starting_idx : ending_idx] ending index not included
+"""
+
+
+
+
+
